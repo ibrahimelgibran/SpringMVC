b77c1a95cd7719368c9b00a4cb63d2293203cf00
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-17 22:43:28 +0700

    update

---
+++ src/main/java/iegcode/wevmvc/model/HelloResponse.java
@@ -0,0 +1,13 @@
+package iegcode.wevmvc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloResponse {
+
+    public String hello;
+}
