7154c2437a473b0ae2ee285d7276fee08c4fbdaf
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-18 20:07:48 +0700

    update

--- src/main/java/iegcode/wevmvc/controller/AuthController.java
+++ src/main/java/iegcode/wevmvc/controller/AuthController.java
@@ -1,13 +1,13 @@
 package iegcode.wevmvc.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AuthController {
@@ -15,12 +15,23 @@ public class AuthController {
     @PostMapping(path = "/auth/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity<String> login(
             @RequestParam(name = "username") String username,
-            @RequestParam(name = "password") String password
+            @RequestParam(name = "password") String password,
+            HttpServletResponse servletResponse
     ){
         if ("Gibran".equals(username) && "rahasia".equals(password)){
+            Cookie cookie = new Cookie("username", username);
+            cookie.setPath("/");
+            servletResponse.addCookie(cookie);
+
             return ResponseEntity.status(HttpStatus.OK).body("OK");
         }else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("KO");
         }
+    }
+
+
+    @GetMapping("/auth/user")
+    public ResponseEntity<String> getUser(@CookieValue("username") String username) {
+        return ResponseEntity.ok("Hello " + username);
     }
 }
--- src/test/java/iegcode/wevmvc/controller/AuthControllerTest.java
+++ src/test/java/iegcode/wevmvc/controller/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package iegcode.wevmvc.controller;
 
+import jakarta.servlet.http.Cookie;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +32,8 @@ class AuthControllerTest {
                         .param("password", "rahasia")
         ).andExpectAll(
                 status().isOk(),
-                content().string(Matchers.containsString("OK"))
+                content().string(Matchers.containsString("OK")),
+                cookie().value("username", Matchers.containsString("Gibran"))
         );
     }
     @Test
@@ -44,6 +46,17 @@ class AuthControllerTest {
         ).andExpectAll(
                 status().isUnauthorized(),
                 content().string(Matchers.containsString("KO"))
+        );
+    }
+
+    @Test
+    void getUser() throws Exception {
+        mockMvc.perform(
+                get("/auth/user")
+                        .cookie(new Cookie("username", "Gibran"))
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hello Gibran"))
         );
     }
 }
