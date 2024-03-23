dbee5d3f0469f8d4fd8ff18a19f1c8c9917f2b75
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-23 12:00:15 +0700

    update

--- src/main/java/iegcode/wevmvc/ServerSpringWebMvcApplication.java
+++ src/main/java/iegcode/wevmvc/ServerSpringWebMvcApplication.java
@@ -2,9 +2,22 @@ package iegcode.wevmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class ServerSpringWebMvcApplication {
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder
+				.setConnectTimeout(Duration.ofSeconds(2L))
+				.setReadTimeout(Duration.ofSeconds(2L))
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerSpringWebMvcApplication.class, args);
